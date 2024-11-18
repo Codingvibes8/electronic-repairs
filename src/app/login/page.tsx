46598d61f228288dc55d5d94567750dce41de1c7
Codingvibes8 <wecodeappz@gmail.com>
2024-11-18 15:22:09 +0000

    add login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
         <main className="h-dvh flex flex-col items-center gap-6 text-4xl p-4">
             <h1> Hyper Electronics Repair Services</h1>
             <Button asChild>
-                <LoginLink className={''}>Sign In</LoginLink>
+                <LoginLink className={'bg-red-700 text-gray-50 '}>Sign In</LoginLink>
             </Button>
         </main>
     )
